b9e3a6bd69aac955898c1c64874c50244539fb56
Benjamin Maidel <benjamin.maidel@mojin-robotics.de>
2020-12-17 12:29:11 +0100

    updated test_client to big endian

--- industrial_robot_client/scripts/test_client.py
+++ industrial_robot_client/scripts/test_client.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import socket
 import sys
 import time
+import struct
 
 import construct as c2
 
@@ -39,49 +40,50 @@ Header = c2.Struct(
 
 SimpleMessage = c2.Struct(
     'Header' / c2.Struct(
-        'msg_type' / c2.Int32sl,
-        'comm_type' / c2.Int32sl,
-        'reply_type' / c2.Int32sl,
+        'msg_type' / c2.Int32sb,
+        'comm_type' / c2.Int32sb,
+        'reply_type' / c2.Int32sb,
     ),
     'body' / c2.Struct(
-        'test' / c2.Float32l,
+        'test' / c2.Float32b,
     ),
     c2.Terminated
 )
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Bind the socket to the port
-server_address = ('localhost', 11002)
-print('starting up on {0[0]} port {0[1]}'.format(server_address))
-sock.connect(server_address)
-
-# Listen for incoming connections
 while True:
-    # Wait for a connection
     try:
+        # Create a TCP/IP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = ('10.0.1.50', 3000)
+        print('connecting on {0[0]} port {0[1]}'.format(server_address))
+        sock.connect(server_address)
+        print("connected")
+
         # Receive the data in small chunks and retransmit it
         while True:
-            
+            print("try reading")            
             data = sock.recv(4)
-            data_len = int(data[0].encode('hex'), 32)
-            print('received {}'.format(data_len))
+            #data_len = int(data[0].encode('hex'), 32)
+            data_len = struct.unpack('>i', data)[0]
+            print('payload data to read {}'.format(data_len))
             data = sock.recv(data_len)
-            print('received {}'.format(data))
             header = SimpleMessage.parse(data)
             print("SimpleMessage: {}".format(header))
 
-            # msg = dict(
-            # Header=dict(msg_type=999, comm_type=1, reply_type=0),
-            # body=dict(test=value))
-            # data = SimpleMessage.build(msg)
-            # #print(feedback_data)
-            # data_len = c2.Int32sl.build(len(data))
-            # print('sending: {}'.format(value))
-            # #self.transport.write(data_len + data)
-            # sock.send(data_len + data)
-
+          # msg = dict(
+          # Header=dict(msg_type=999, comm_type=1, reply_type=0),
+          # body=dict(test=value))
+          # data = SimpleMessage.build(msg)
+          # #print(feedback_data)
+          # data_len = c2.Int32sl.build(len(data))
+          # print('sending: {}'.format(value))
+          # #self.transport.write(data_len + data)
+          # sock.send(data_len + data)
+    except Exception as e: 
+        print("Error communicating: {}".format(e))
+        pass
     finally:
         # Clean up the connection
         sock.close()
+        time.sleep(2)
